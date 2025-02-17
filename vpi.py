263b4a7579eb5c80c4d45bf3bc5ee4d53f51e9f7
Alexandre DOYEN <alex.doyen20042000@gmail.com>
2025-02-17 19:28:27 +0100

    Checkpoint

---
+++ vpi.py
@@ -0,0 +1,12 @@
+import requests
+
+def get():
+    url = "https://prim.iledefrance-mobilites.fr/marketplace/estimated-timetable?LineRef=STIF%3ALine%3A%3AC00680%3A"
+    headers = {
+        "accept": "application/json",
+        "apiKey": "i2Mkal2UiaNMGH9qK5giHc503kxffJIe"
+    }
+
+    return requests.get(url, headers = headers).json()
+
+get()
